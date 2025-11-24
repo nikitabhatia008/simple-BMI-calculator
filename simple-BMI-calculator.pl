df05bb40d70196afc2c1229285200958f4c07d44
nikitabhatia008 <nikita.25bcy10176@vitbhopal.ac.in>
2025-11-25 00:32:10 +0530

    Create simple-BMI-calculator.pl

---
+++ simple-BMI-calculator.pl
@@ -0,0 +1,15 @@
+weight = float(input("Enter your weight in kg: "))
+height = float(input("Enter your height in meters: "))
+bmi = weight / (height ** 2)
+
+print("Your BMI is:", round(bmi, 2))
+
+if bmi < 18.5:
+    print("You are underweight.")
+elif bmi < 24.9:
+    print("You have normal weight.")
+elif bmi < 29.9:
+    print("You are overweight.")
+else:
+    print("You are obese.")
+    
